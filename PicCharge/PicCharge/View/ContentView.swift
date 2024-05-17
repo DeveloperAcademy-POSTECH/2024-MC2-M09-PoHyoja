2849f1c03621d499a86f84e5d866dd382c32db2d
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-05-17 14:21:56 +0900

    FEAT: #5 ContentView 로그인 분기 처리 화면 구현

--- PicCharge/PicCharge/View/ContentView.swift
+++ PicCharge/PicCharge/View/ContentView.swift
@@ -8,21 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum ViewState {
+        case loginFailed
+        case roleNotSelected
+        case notConnected
+        case connected
+    }
+    
     @State private var navigationManager = NavigationManager()
+    @State private var viewState: ViewState = .loginFailed
+    
+    @State private var role: Role = .child
+    @State private var isAutoLogined: Bool = false
+    @State private var isRoleSelected: Bool = false
+    @State private var isConnected: Bool = false
     
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
             VStack {
-                // TODO: - 자식 부모 플로우 분리 필요
-                Button("로그인 성공 -> 탭화면") {
-                    navigationManager.path.append(.childTab)
-                }
+                Text("여기는 실제로 보이지 않는 화면입니다.")
                 
-                Button("로그인 실패 -> 로그인화면") {
+                Picker("부모자식 역할", selection: $role) {
+                    ForEach(Role.allCases, id: \.self) { role in
+                        Text("\(role)")
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Toggle("자동 로그인 성공", isOn: $isAutoLogined)
+                Toggle("역할 선택 완료", isOn: $isRoleSelected)
+                Toggle("부모 자식 연결 완료", isOn: $isConnected)
+                
+                Text("로그인 실패")
+                Button("로그인 화면으로 이동") {
                     navigationManager.path.append(.login)
                 }
+                .disabled(isAutoLogined)
+                
+                Text("로그인 성공")
+                Button("역할선택화면으로 이동") {
+                    navigationManager.path.append(.selectRole)
+                }
+                .disabled(!(isAutoLogined && !isRoleSelected && !isConnected))
+                
+                Text("로그인 성공 & 역할 선택 완료")
+                Button("유저연결화면으로 이동") {
+                    navigationManager.path.append(.connectUser)
+                }
+                .disabled(!(isAutoLogined && isRoleSelected && !isConnected))
+                
+                Text("로그인 성공 & 역할 선택 완료 & 연결 완료")
+                Button("\(role) 메인화면으로 이동") {
+                    navigationManager.path.append(role == .child ? .childTab : .parentAlbum)
+                }
+                .disabled(!(isAutoLogined && isRoleSelected && isConnected))
             }
-            
             .navigationDestination(for: PathType.self) { path in
                 switch path {
                     // MARK: - 초기 설정
