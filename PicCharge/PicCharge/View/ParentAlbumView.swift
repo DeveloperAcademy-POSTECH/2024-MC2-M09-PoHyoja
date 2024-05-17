80e97f1673329367cb3e411ab8c8ddc2afa2e4dd
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-05-17 22:46:07 +0900

    FEAT: #5 부모 설정화면 이동 로직 추가

--- PicCharge/PicCharge/View/ParentAlbumView.swift
+++ PicCharge/PicCharge/View/ParentAlbumView.swift
@@ -13,6 +13,9 @@ struct ParentAlbumView: View {
     var body: some View {
         VStack {
             Text("부모 앨범 화면")
+            Button("설정") {
+                navigationManager.push(to: .setting)
+            }
             List {
                 Button("사진 1") {
                     navigationManager.push(to: .parentAlbumDetail)
