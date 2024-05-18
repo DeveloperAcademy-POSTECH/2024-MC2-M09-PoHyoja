21cb9ece2b2e0fb0b69464a95f8048ea19450035
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-05-19 00:20:04 +0900

    Feat: #7 Icon enum 추가

---
+++ PicCharge/PicCharge/Global/Icon.swift
@@ -0,0 +1,21 @@
+//
+//  Icon.swift
+//  PicCharge
+//
+//  Created by 남유성 on 5/19/24.
+//
+
+import Foundation
+
+public enum Icon {
+    static let camera = "camera"
+    static let addPhoto = "photo.badge.plus"
+    static let album = "photo"
+    static let heartBolt = "bolt.heart"
+    static let heart = "heart.fill"
+    static let menu = "ellipsis.circle"
+    static let setting = "gearshape.fill"
+    static let rotate = "arrow.triangle.2.circlepath"
+    static let close = "xmark"
+    static let light = "bolt.fill"
+}
