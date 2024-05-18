85f648ec0161c23ccf3fe35bcfb8046997427c14
GitDohyun <gitdohyun@gmail.com>
2024-05-19 01:22:45 +0900

    #18 Feat: #18 위젯 기본 설정 구현

---
+++ PicCharge/ParentWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  ParentWidget
+//
+//  Created by 김도현 on 5/19/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
