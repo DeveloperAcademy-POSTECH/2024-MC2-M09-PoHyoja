4115258ee63f0748823944a2cd3eae814125b232
mosiccan <mosiccan@gmail.com>
2024-05-19 20:24:02 +0900

    Feat: #23 TempChildSelectGalleryView 들여쓰기 수정, MARK 표시

--- PicCharge/PicCharge/View/TempChildSelectGalleryView.swift
+++ PicCharge/PicCharge/View/TempChildSelectGalleryView.swift
@@ -35,6 +35,8 @@ struct GalleryPicker: UIViewControllerRepresentable {
 
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             picker.dismiss(animated: true)
+            
+            // MARK: 이미지 선택시 jpeg으로 압축
             if let image = info[.originalImage] as? UIImage {
                 parentGalleryPicker.selectedImageData = image.jpegData(compressionQuality: 1.0)
             }
@@ -47,20 +49,23 @@ struct GalleryPicker: UIViewControllerRepresentable {
 }
 
 struct TempChildSelectGalleryView: View {
+    // MARK: 아래 두 변수는 NavigationPath 적용 전 NavigationStack으로 이미지를 넘겨줄 때 사용되는 변수
+    // MARK: 실제 작업 시 삭제 혹은 수정이 필요
     @State private var selectedImageData: Data? = nil
     @State private var navigateToSendGalleryView = false
     
     var body: some View {
-        // 일단 Temp파일로서, NavigationStack으로 사용
-        NavigationStack { 
+        // MARK: Temp파일로, NavigationStack을 사용,
+        NavigationStack {
             VStack {
                 if let selectedImageData,
-                   let uiImage = UIImage(data: selectedImageData) {                    NavigationLink(destination: TempChildSendGalleryView(imageData: $selectedImageData), isActive: $navigateToSendGalleryView) {
-                        EmptyView()
-                    }
-                    .onAppear() {
-                        navigateToSendGalleryView = true
-                    }
+                   let uiImage = UIImage(data: selectedImageData) {                    
+                    NavigationLink(destination: TempChildSendGalleryView(imageData: $selectedImageData), isActive: $navigateToSendGalleryView) {
+                       EmptyView()
+                   }
+                   .onAppear() {
+                       navigateToSendGalleryView = true
+                   }
                 } else {
                     GalleryPicker(selectedImageData: $selectedImageData)
                         .frame(maxHeight: .infinity)
