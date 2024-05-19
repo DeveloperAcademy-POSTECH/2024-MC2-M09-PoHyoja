42a60b5c406f2d7f8baa60d10332da6f34721f78
mosiccan <mosiccan@gmail.com>
2024-05-19 20:56:14 +0900

    Feat: #34 TempChildSendGalleryView로 이미지 넘겨주는 방식 수정
    - GalleryPickerView로 이름 변경
    - .frame(), Spacer() 중복으로 인해 .ignoresSafeArea()로 구현
    - TempChildSendGalleryView의 프리뷰는 Data 타입으로 넣기 애매하여 지웠습니다.
    - .preferredColorScheme(.dark)는 App단에서 진행하면 되기에 제거

--- PicCharge/PicCharge/View/TempChildSendGalleryView.swift
+++ PicCharge/PicCharge/View/TempChildSendGalleryView.swift
@@ -9,40 +9,31 @@ import SwiftUI
 import PhotosUI
 
 struct TempChildSendGalleryView: View {
-    @Binding var imageData: Data?
+    let imageData: Data
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
         VStack {
-            if let imageData = imageData,
-               let uiImage = UIImage(data: imageData) {
+            if let uiImage = UIImage(data: imageData) {
                 Image(uiImage: uiImage)
                     .resizable()
                     .scaledToFill()
                     .frame(width: 360, height: 360)
                     .clipShape(RoundedRectangle(cornerRadius: 21))
-                    .clipped()
-            } else {
-                Text("No image selected")
-                    .padding()
             }
         }
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItemGroup(placement: .bottomBar) {
                 Button("다시 선택하기") {
+                    // MARK: 다른 로직 필요, - NavigationPath 적용전 이전 뷰로 돌아가기 위한 dismiss()입니다.
                     dismiss()
                 }
                 .foregroundStyle(.white)
                 Button("사진 보내기") {
+                    // MARK: NavigationPath 사용 시 로딩뷰로 넘어가야 합니다.
                 }
             }
         }
-        .preferredColorScheme(.dark)
     }
 }
-
-#Preview {
-    TempChildSendGalleryView(imageData: .constant(nil))
-}
-
