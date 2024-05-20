9c1591495e65e2bb27945a98c511fa0a68272e79
GitDohyun <gitdohyun@gmail.com>
2024-05-21 01:08:43 +0900

    Feat: #20 위젯 이미지 변경 기능 구현

--- PicCharge/ParentWidget/ParentWidget.swift
+++ PicCharge/ParentWidget/ParentWidget.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 import WidgetKit
 
+
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let image: UIImage
 }
 
-struct ImageProvider: TimelineProvider {
+struct ParentWidgetView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Image(uiImage: entry.image)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .containerBackground(Color.clear, for: .widget)
+    }
+}
+
+class Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), image: UIImage())
     }
@@ -25,14 +37,21 @@ struct ImageProvider: TimelineProvider {
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
         var entries: [SimpleEntry] = []
-        let url = URL(string: "https://img.movist.com/?img=/x00/00/00/20_p1.jpg")!
+        var timeline = Timeline(entries: entries, policy: .atEnd)
+        
+        guard let urlString = UserDefaults.shared.string(forKey: "urlString"),
+              let url = URL(string: urlString)
+        else {
+            completion(timeline)
+            return
+        }
 
         let task = URLSession.shared.dataTask(with: url) { (data, _, _) in
             if let data = data, let image = UIImage(data: data) {
                 let entry = SimpleEntry(date: Date(), image: image)
                 entries.append(entry)
                 
-                let timeline = Timeline(entries: entries, policy: .atEnd)
+                timeline = Timeline(entries: entries, policy: .atEnd)
                 completion(timeline)
             }
         }
@@ -40,18 +59,32 @@ struct ImageProvider: TimelineProvider {
     }
 }
 
+
 struct ParentWidget: Widget {
     let kind: String = "ParentWidget"
-
+    
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: ImageProvider()) { entry in
-            Image(uiImage: entry.image)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            ParentWidgetView(entry: entry)
         }
         .configurationDisplayName("PicCharge")
         .description("필요하면 나중에 설명 추가하기")
         .supportedFamilies([.systemLarge])
+        .contentMarginsDisabled()
     }
 }
+
+
+#Preview(as: .systemSmall) {
+    ParentWidget()
+} timeline: {
+    SimpleEntry(date: .now, image: UIImage())
+    SimpleEntry(date: .now, image: UIImage())
+}
+
+
+
+
+
+
 
