3fb9ed6de4fe67559868fb3d9239100c27d6d067
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-05-21 03:20:25 +0900

    Feat: #42 TempChildAlbumView 합치기

---
+++ PicCharge/PicCharge/View/Components/AsyncImageView.swift
@@ -0,0 +1,51 @@
+//
+//  AsyncImageView.swift
+//  PicCharge
+//
+//  Created by 남유성 on 5/21/24.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+    @State private var image: UIImage? = nil
+    private let urlString: String
+    
+    init(urlString: String) {
+        self.urlString = urlString
+    }
+
+    var body: some View {
+        Group {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+            } else {
+                ProgressView()
+            }
+        }
+        .task {
+            await loadImage()
+        }
+    }
+    
+    private func loadImage() async {
+        guard let url = URL(string: urlString) else {
+            print("Error convert string to URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let loadedImage = UIImage(data: data) {
+                self.image = loadedImage
+            }
+        } catch {
+            print("Error loading image: \(error)")
+        }
+    }
+}
+#Preview {
+    AsyncImageView(urlString: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsNICnidsWi7x-UmXHlkEz-8VUeKwmJSg86Xli4i-26A&s")
+}
