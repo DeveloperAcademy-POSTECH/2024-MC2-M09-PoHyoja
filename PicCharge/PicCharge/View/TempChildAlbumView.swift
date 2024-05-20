d0052721bdaea62063fc6df7a34e0172914994bd
Byhoon kim (Miles) <by0881@naver.com>
2024-05-20 11:11:51 +0900

    Feat: #16 GeometryReader 넣었습니다.
    LazyVGrid에서 spacing을 0으로 지정하고 griditem 너비는 width/3으로 지정 했을때 사진 사이 간격이 생깁니다.  vgrid spacing을 3으로 맞춰서 임시로 해결 했습니다.

--- PicCharge/PicCharge/View/TempChildAlbumView.swift
+++ PicCharge/PicCharge/View/TempChildAlbumView.swift
@@ -9,59 +9,73 @@ import SwiftUI
 
 struct TempChildAlbumView: View {
     
-    let columnLayout = Array(repeating: GridItem(), count: 3)
+    //geometryReader로 3등분
+    let columnLayout = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    //끝도없이 내려가는거 확인해보기
+    
     let photos = Photo.mockup
     
     //날짜형식을 위해 formatter를 만들어줍니다.
     static let dateformat: DateFormatter = {
-          let formatter = DateFormatter()
-           formatter.dateFormat = "YYYY년 M월 d일"
-           return formatter
-       }()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY년 M월 d일"
+        return formatter
+    }()
     var today = Date()
     
     var body: some View {
         NavigationStack{
-            ScrollView{
-                VStack(alignment: .leading){
-                    Text("최근 업로드 사진")
-                        .font(.headline)
-                    
-                    AsyncImage(url: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsNICnidsWi7x-UmXHlkEz-8VUeKwmJSg86Xli4i-26A&s")) { image in
-                        image.resizable()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .cornerRadius(10.0)
-                    Text("\(today, formatter: TempChildAlbumView.dateformat)")
-                    
-                    Divider()
-                    
-                    Text("충전 기록")
-                    LazyVGrid(columns: columnLayout, spacing: 0) {
-                        ForEach(photos) { photo in
-                            AsyncImage(url: URL(string: photo.urlString)) { image in
-                                image.resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 120, height: 120)
-                                    .clipped()
-                            } placeholder: {
-                                ProgressView()
+            GeometryReader {geometry in
+                ScrollView{
+                    VStack(alignment: .leading, spacing: 8){
+                        Text("최근 업로드 사진")
+                            .font(.headline)
+                        
+                        AsyncImage(url: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsNICnidsWi7x-UmXHlkEz-8VUeKwmJSg86Xli4i-26A&s")) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipped()
+                        } placeholder: {ProgressView()}
+                        .frame(width: geometry.size.width , height: geometry.size.width)
+                        .cornerRadius(10.0)
+                        
+                        Text("\(today, formatter: TempChildAlbumView.dateformat)")
+                            .font(.subheadline)
+                        Divider()
+                            .padding(.vertical, 8)
+                        Text("충전 기록")
+                            .font(.headline)
+            
+                        LazyVGrid(columns: columnLayout, spacing: 3) {
+                            ForEach(photos) { photo in
+                                AsyncImage(url: URL(string: photo.urlString)) { image in
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: (geometry.size.width / 3), height: (geometry.size.width / 3))
+                                        .clipped()
+                                } placeholder: {ProgressView()}
                             }
                         }
+                        .cornerRadius(8.0)
+                        
                     }
-                    .cornerRadius(8.0)
                 }
             }
             .padding()
             .navigationTitle("앨범")
+            
         }
+        
         
         
     }
     
 }
-
 
 
 #Preview {
