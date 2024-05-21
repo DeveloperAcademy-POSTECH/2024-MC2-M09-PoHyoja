42b7d5337e29f15029358b5f09fafc2a5cf70096
Byhoon kim (Miles) <by0881@naver.com>
2024-05-21 12:34:55 +0900

    Feat: #49 로딩 애니메이션 수정, 배경 추가
    메인뷰와 맞춰서 배경을 추가하고, 로딩애니메이션을 수정했습니다.

--- PicCharge/PicCharge/View/LoadingView.swift
+++ PicCharge/PicCharge/View/LoadingView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        VStack {
-            LottieView(jsonName: "TestAnimation", loopMode:.loop)
-                .frame(width: 200, height: 200)  // Adjust the frame size as needed
+        ZStack{
+            VStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.bgGreen, Color.bgGreen.opacity(0)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .frame(height: 320)
+                
+                Spacer()
+            }
+            .ignoresSafeArea()
+            VStack {
+                LottieView(jsonName: "BatteryLoading", loopMode: .loop)
+                    .frame(width: 200, height: 200)  // Adjust the frame size as needed
+            }
         }
     }
 }
-
 #Preview {
     LoadingView()
 }
