133d03794e5e208422bebc1cbda0a1376b88ebef
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-05-23 22:58:47 +0900

    Feat: #70 회원가입 및 유저 연결화면 구현

---
+++ PicCharge/PicCharge/View/SelectRoleForSignUpView.swift
@@ -0,0 +1,150 @@
+//
+//  SelectRoleForSignUpView.swift
+//  PicCharge
+//
+//  Created by 남유성 on 5/23/24.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct SelectRoleForSignUpView: View {
+    @Environment(NavigationManager.self) var navigationManager
+    
+    @State var selectedRole: Role = .child
+    @State private var isNetworking: Bool = false
+    
+    private let name: String
+    private let email: String
+    private let password: String
+        
+    var isSignUpAvailable: Bool {
+        !name.isEmpty && !email.isEmpty && !password.isEmpty
+    }
+    
+    init(name: String, email: String, password: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("당신의 역할을 선택해주세요")
+                    .font(.title2.bold())
+                    .foregroundStyle(.txtPrimaryDark)
+                    .padding(.top, 40)
+                
+                Spacer()
+            }
+            
+            Spacer()
+            
+            HStack(spacing: 60) {
+                VStack(spacing: 10) {
+                    ZStack {
+                        Image("자식")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .clipShape(Circle())
+                        
+                        if selectedRole == .child {
+                            Circle()
+                                .stroke(Color.accent, lineWidth: 4)
+                                .frame(width: 100)
+                                .transition(.identity)
+                        }
+                    }
+                    .frame(width: 100)
+                    
+                    Text("자식")
+                        .font(.title3)
+                        .foregroundStyle(.txtPrimaryDark)
+                }
+                .onTapGesture {
+                    selectedRole = .child
+                }
+                
+                VStack(spacing: 10) {
+                    ZStack {
+                        Image("부모")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .clipShape(Circle())
+                        
+                        if selectedRole == .parent {
+                            Circle()
+                                .stroke(Color.accent, lineWidth: 4)
+                                .frame(width: 100)
+                                .transition(.identity)
+                        }
+                    }
+                    .frame(width: 100)
+                    
+                    Text("부모")
+                        .font(.title3)
+                        .foregroundStyle(.txtPrimaryDark)
+                }
+                .onTapGesture {
+                    selectedRole = .parent
+                }
+                
+            }
+            
+            Spacer()
+            Spacer()
+            
+            Button {
+                Task {
+                    isNetworking = true
+                    await signUp(name: name, email: email, password: password, role: selectedRole)
+                    isNetworking = false
+                }
+            } label: {
+                ZStack {
+                    isSignUpAvailable ? Color.green : Color.gray
+                    
+                    if isNetworking {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    } else {
+                        Text("회원가입")
+                            .bold()
+                            .foregroundStyle(.txtPrimaryDark)
+                    }
+                }
+            }
+            .frame(height: 54)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .disabled(isNetworking)
+        }
+        .padding(.horizontal, 16)
+        .padding(.bottom, 16)
+        .navigationTitle("역할 선택")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+extension SelectRoleForSignUpView {
+    func signUp(name: String, email: String, password: String, role: Role) async {
+        do {
+            let authResult = try await Auth.auth().createUser(withEmail: email, password: password)
+            let user = authResult.user
+            
+            let newUser = User(id: user.uid, name: name, role: role, email: email, connectedTo: [])
+            try await FirestoreService.shared.addUser(user: newUser)
+            
+            navigationManager.popToRoot()
+        } catch {
+            print("회원 가입 실패")
+            navigationManager.pop(to: .signUp(errorMessage: "회원 가입 실패"))
+        }
+    }
+}
+
+#Preview {
+    SelectRoleForSignUpView(name: "", email: "", password: "")
+        .environment(NavigationManager())
+        .preferredColorScheme(.dark)
+}
