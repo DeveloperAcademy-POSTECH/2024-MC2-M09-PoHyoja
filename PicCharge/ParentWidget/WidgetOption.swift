0007996a1bf68a36c377dc45890a4aa7c769ad65
GitDohyun <gitdohyun@gmail.com>
2024-05-25 14:27:50 +0900

    Feat: #87 자식 위젯 배터리 감소 구현 및 자식 위젯 옵션 개별 파일로 분리

---
+++ PicCharge/ParentWidget/WidgetOption.swift
@@ -0,0 +1,22 @@
+//
+//  WidgetOption.swift
+//  PicCharge
+//
+//  Created by 김도현 on 5/22/24.
+//
+
+import Foundation
+
+struct ChildWidgetOption {
+    // 목표로 설정한 시간 설정(byAdding에 따라 초, 분 시간 등이 될 수 있음)
+    // ex)totalTime = 150이고, byAdding이 .hour이면 목표로 설정한 시간이 150시간이 됨
+    static let totalTime = 13
+
+    // 사진을 전송한 시간 설정
+    static let photoSentDate = Date()
+
+    // byAdding의 .second를 변경하여 분, 시간 등으로 시간 측정 단위를 변경 가능
+    static func byAdding(value: Int, to date: Date) -> Date {
+        return Calendar.current.date(byAdding: .second, value: value, to: date)!
+    }
+}
