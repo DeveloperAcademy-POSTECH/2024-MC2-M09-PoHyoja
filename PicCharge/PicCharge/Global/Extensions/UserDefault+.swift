47b3157611315d526e83fd033501a4a8387d90fe
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-09-23 05:32:42 +0900

    Chore: Debug/Release build settings

--- PicCharge/PicCharge/Global/Extensions/UserDefault+.swift
+++ PicCharge/PicCharge/Global/Extensions/UserDefault+.swift
@@ -9,7 +9,8 @@ import Foundation
 
 extension UserDefaults {
     static var shared: UserDefaults {
-        let appGroupId = "group.com.pohyoja.PicCharge"
+        let bundle = Bundle.main.bundleIdentifier
+        let appGroupId = "group." + (bundle ?? "com.pohyoja.PicCharge")
         return UserDefaults(suiteName: appGroupId)!
     }
 }
